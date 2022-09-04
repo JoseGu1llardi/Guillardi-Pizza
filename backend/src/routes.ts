d0613797a7b5d1f33700ecab7cdaee1896a38b26
JoseGu1llardi <junior11_junior@hotmail.com>
2022-09-04 15:33:30 +0100

    creating controllers and services

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,9 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
+
+import { CreateUserController } from './controllers/user/CreateUserController';
 
 const router = Router();
 
-router.get('/teste', (req: Request, res: Response) => {
-    //throw new Error('Error while making this request');
-    return res.json({ name: 'Sujeito Pizza' });
-});
+router.post('/users', new CreateUserController().handle)
 
 export { router };
