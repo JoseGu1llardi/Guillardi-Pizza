596951ef85a562e602154da05397960794dd71c9
JoseGu1llardi <junior11_junior@hotmail.com>
2022-10-21 14:36:35 +0100

    ...

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Guillardi Pizza - Login</title>
+      </Head>
+      <div>
+        <h1>Guillardi Pizzaria</h1>
+      </div>
+    </>
+  )
+}
