4bc47ca8182d8265f9ab372002ad660023098ac1
JoseGu1llardi <junior11_junior@hotmail.com>
2022-11-19 13:40:43 +0000

    ...

--- mobile/src/pages/Order/index.tsx
+++ mobile/src/pages/Order/index.tsx
@@ -1,6 +1,16 @@
-import { SafeAreaView, Text, } from 'react-native';
+import {
+    SafeAreaView,
+    Text,
+    View,
+    TouchableOpacity,
+    TextInput
+} from 'react-native';
 
 import { useRoute, RouteProp } from '@react-navigation/native';
+
+import { Feather } from '@expo/vector-icons';
+
+import { styles } from './styles';
 
 type RouteDetailParams = {
     Order: {
@@ -18,11 +28,35 @@ export default function Order() {
     const { table, order_id, clientName } = route.params;
 
     return (
-        <SafeAreaView>
-            <Text>Screen Order</Text>
-            <Text>{ table }</Text>
-            <Text>{ order_id }</Text>
-            <Text>{ clientName }</Text>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.title}>Table {route.params.table}</Text>
+                <TouchableOpacity>
+                    <Feather name='trash-2' size={28} color='#FF3F4B' />
+                </TouchableOpacity>
+            </View>
+
+            <TouchableOpacity style={styles.input}>
+                <Text style={{ color: '#FFF' }}>Pizzas</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.input}>
+                <Text style={{ color: '#FFF' }}>Pepperoni pizza</Text>
+            </TouchableOpacity>
+
+            <View style={styles.qtdContainer}>
+                <Text style={styles.qtdText}>Quantity</Text>
+                <TextInput
+                    style={[styles.input,
+                    {
+                        width: '60%', textAlign: 'center',
+                    }]}
+                    placeholderTextColor='#FFF'
+                    keyboardType='numeric'
+                    value='1'
+                />
+            </View>
+
         </SafeAreaView>
     )
 }
--- mobile/src/pages/Order/styles.ts
+++ mobile/src/pages/Order/styles.ts
@@ -2,6 +2,43 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
     container: {
-        
+        flex: 1,
+        backgroundColor: '#1D1D2E',
+        paddingVertical: '20%',
+        paddingEnd: '4%',
+        paddingStart: '4%',
+    },
+    header: {
+        flexDirection: 'row',
+        marginBottom: 12,
+        alignItems: 'center',
+        marginTop: 24
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#FFFFFF',
+        marginRight: 14,
+    },
+    input: {
+        backgroundColor: '#101026',
+        borderRadius: 4,
+        width: '100%',
+        height: 40,
+        marginBottom: 12,
+        justifyContent: 'center',
+        paddingHorizontal: 8, 
+        color: '#FFF',
+        fontSize: 20,
+    },
+    qtdContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    qtdText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#FFF'
     }
 });
